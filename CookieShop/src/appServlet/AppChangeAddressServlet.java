cb84f490b19b682e5a4529c5bbb70feffb46a121
coldwarm7 <1259404192@qq.com>
2019-04-02 18:34:16 +0800

    json fix

--- CookieShop/src/appServlet/AppChangeAddressServlet.java
+++ CookieShop/src/appServlet/AppChangeAddressServlet.java
@@ -2,6 +2,7 @@ package appServlet;
 
 
 import com.alibaba.fastjson.JSON;
+import com.google.gson.Gson;
 import model.Msg;
 import model.User;
 import net.sf.json.JSONObject;
@@ -16,6 +17,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
@@ -29,7 +31,53 @@ import static java.lang.System.out;
 public class AppChangeAddressServlet extends HttpServlet {
 
     private UserService uService = new UserService();
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
 
+
+    //json
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json; charset=utf-8");
+        response.setCharacterEncoding("UTF-8");
+//        String data = getPostData(request);
+//        out.println(data);
+
+        //data = data.substring(1, data.length()-1);
+        //User loginUser = new Gson().fromJson(data, User.class);
+        JSONObject json=JsonReader.receivePost(request);
+        User loginUser = (User) JSONObject.toBean(json,User.class);
+        uService.updateUserAddress(loginUser);
+        loginUser = uService.selectById(loginUser.getId());
+        List<User> users = new ArrayList<>();
+        users.add(loginUser);
+
+        Msg msg = null;
+        if (loginUser != null){
+            msg = new Msg(true,"修改用户地址成功",users);
+        }else {
+            msg = new Msg(false,"修改用户地址失败",users);
+        }
+
+        String registerJson = JSON.toJSONString(msg);
+        OutputStream out = response.getOutputStream();
+        out.write(registerJson.getBytes("UTF-8"));
+        out.flush();
+    }
+
+    //    private static String getPostData(HttpServletRequest request) {
+//        StringBuffer data = new StringBuffer();
+//        String line = null;
+//        BufferedReader reader = null;
+//        try {
+//            reader = request.getReader();
+//            while (null != (line = reader.readLine()))
+//                data.append(line);
+//        } catch (IOException e) {
+//        } finally {
+//        }
+//        return data.toString();
+//    }
     //from-data
 //    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //
@@ -76,31 +124,5 @@ public class AppChangeAddressServlet extends HttpServlet {
 //
 //    }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doPost(request,response);
-    }
 
-    //json
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("application/json; charset=utf-8");
-        response.setCharacterEncoding("UTF-8");
-        JSONObject json=JsonReader.receivePost(request);
-        User loginUser = (User) JSONObject.toBean(json,User.class);
-        uService.updateUserAddress(loginUser);
-        loginUser = uService.selectById(loginUser.getId());
-        List<User> users = new ArrayList<>();
-        users.add(loginUser);
-
-        Msg msg = null;
-        if (loginUser != null){
-             msg = new Msg(true,"修改用户地址成功",users);
-        }else {
-            msg = new Msg(false,"修改用户地址失败",users);
-        }
-
-        String registerJson = JSON.toJSONString(msg);
-        OutputStream out = response.getOutputStream();
-        out.write(registerJson.getBytes("UTF-8"));
-        out.flush();
-    }
 }
